2e829d964603ad4091fc6f52190e0f74eb7f39a1
Tu Nombre <you@example.com>
2024-05-27 20:48:05 +0200

    terminado el listado

--- read.php
+++ read.php
@@ -1,7 +1,53 @@
 <?php
+    include 'db.php';
     include 'header.php';
+    $sql = "SELECT id, nombre, telefono, correo, mensaje FROM contactos";
+    $result=$conn->query($sql);
+
+    
 ?>
 <h1>leer mensajes</h1>
+
+<div class="contenedor listado">
+    <div class="fila cabecera">
+        <div class="campoCabecera">
+            id
+        </div>
+        <div class="campoCabecera">
+            Nombre
+        </div>
+        <div class="campoCabecera">
+            telefono
+        </div>
+        <div class="campoCabecera">
+            Correo
+        </div>
+        <div class="campoCabecera">
+            Mensaje
+        </div>
+    </div>
+    <?php
+        $num=0;
+        while($row = $result->fetch_assoc()){
+            $num++;
+            $paridad="impar";
+            if($num%2==0){
+                $paridad="par";
+            }
+            //PARIDAD ME INDICA SI LA FILA ES PAR O IMPAR
+            ?>
+            <div class="fila <?=$paridad?>">
+                    <div class="campo"><?=$row['id']?></div>
+                    <div class="campo"><?=$row['nombre']?></div>
+                    <div class="campo"><?=$row['telefono']?></div>
+                    <div class="campo"><?=$row['correo']?></div>
+                    <div class="campo"><?=$row['mensaje']?></div>
+            </div>
+
+        <?php
+        }
+    ?>
+</div>
 
 <?php
     include 'footer.php';
